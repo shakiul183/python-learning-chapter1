28e7d653d9d43805745ba7a105fc94dce5a5cd54
shakiul karim <shakiulkarim8@gmail.com>
2025-10-20 22:56:50 +0600

    String Function

---
+++ string_function.py
@@ -0,0 +1,13 @@
+name = "shakiul karim"
+print(len(name))
+print(name.endswith("rim"))  # check that string complete end or not
+print(name.startswith("sha")) # check that String complete Start or not
+
+print(name.capitalize()) # capitalize Start character
+print(name.lower())
+print(name.upper())
+
+a = "shakiul is a good man"
+print(a.replace("good","bad")) # Replace a String index
+
+  
