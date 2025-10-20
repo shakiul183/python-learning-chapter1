2ea086b9c7bb9a118ec6439657045b845db68445
shakiul karim <shakiulkarim8@gmail.com>
2025-10-20 07:29:22 +0600

    type casting

--- type_casting.py
+++ type_casting.py
@@ -1,7 +1,14 @@
 a = "32.2"
-b = float(a) # convert Strings to 
+b = float(a) # convert Strings to  float
 print(b)
 t1 = type(b)
 print(t1)
 t = type(a)
 print (t)
+
+d = 233.7777
+t2 = type(d)
+print(t2)
+e = str(d)
+t3 = type(e)
+print(t3)
