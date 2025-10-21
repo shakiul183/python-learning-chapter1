5bcb6f61dc82478e47ba053e1b59c95b26da6d5a
shakiul karim <shakiulkarim8@gmail.com>
2025-10-22 04:52:03 +0600

    list fundamental

---
+++ list.py
@@ -0,0 +1,5 @@
+name = ["shakiul","karim","shohan","10.2365","1245852"] # this is list not string. string can not be change but list can be change
+print(name[4])
+name[0]="shakiul karim"
+print(name[0])
+print(name[1:4])
