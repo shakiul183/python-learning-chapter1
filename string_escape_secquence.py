610a2bd590f86c62bb68bc4978a0659b7dacccc5
shakiul karim <shakiulkarim8@gmail.com>
2025-10-21 07:13:16 +0600

    strings printing

--- string_escape_secquence.py
+++ string_escape_secquence.py
@@ -1,3 +1,5 @@
 a= "shakiul karim is a\nskilled engineer \" of electrical and electronic engineering\" "
 print(a)
 # jodi double coate soho print korte chai tahole \"\"  eivabe sajate hobe 
+b = "my dream is \t embedded system design " #for a tab
+print(b)
