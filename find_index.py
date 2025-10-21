a91de87fa538b2fe973f788eb7058d965a86bff1
shakiul karim <shakiulkarim8@gmail.com>
2025-10-21 22:37:14 +0600

    you can find a specific index and then you can replace

---
+++ find_index.py
@@ -0,0 +1,11 @@
+name = "hello i am shakiul karim  i am an engineer"
+print(name.find("  "))  # if parent string has a double  space then  .find function will return that  character number of index
+
+# or you can find.
+print(name.find("engineer"))
+
+# use .find function and  replace that  specific word
+
+print(name.replace ("  "," ")) # to replace double space to single space
+
+ 
