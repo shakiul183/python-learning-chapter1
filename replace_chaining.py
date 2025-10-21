44b4773eff1992f4a7c3c29b49a28b398f010d53
shakiul karim <shakiulkarim8@gmail.com>
2025-10-21 07:48:37 +0600

    replace chain method

---
+++ replace_chaining.py
@@ -0,0 +1,8 @@
+letterr= """
+Dear <|name|>,
+You are Selected for the interview!,
+<|date|>,
+<|year|>
+"""
+print(letterr.replace("<|name|>","shakiul").replace("<|date|>","10.21.2024").replace("<|year|>","2050"))
+# replace chain method
