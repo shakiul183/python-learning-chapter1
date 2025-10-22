1add36794a5fac8b6944537c76690a83792b8868
shakiul karim <shakiulkarim8@gmail.com>
2025-10-23 03:12:51 +0600

    how to sum in tuple

--- tuple_sum.py
+++ tuple_sum.py
@@ -1,3 +1,5 @@
 l = (1,55,55,545,66,)
 print(type(l))
-print(sum(l))
+print(sum(l)) # sum is a built in function for add integer of float
+
+print(l.count(55))   # count is a built in function to count specific
