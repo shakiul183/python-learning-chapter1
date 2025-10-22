0adb30f7617754d71d6780246f796d4fc1d12495
shakiul karim <shakiulkarim8@gmail.com>
2025-10-23 02:22:20 +0600

    how to pop insert remove

---
+++ list_remove.py
@@ -0,0 +1,17 @@
+name = ["shakiul","karim","shohan","10.2365","1245852"] # this is list not string. string can not be change but list can be change
+name.append("daffodil international university")
+print(name)
+
+l1=[1,555,544,54,225,858,45454,2154,]
+l1.sort()
+print(l1) # print sort
+l1.insert(3,3333) # insert such that its index in the list is 3
+print(l1)
+l1.pop(4) # pop means : pop can delete a index value and return changed
+print(l1.pop(4)) # pop and pop value will return
+
+
+print(l1)
+
+l1.remove(1)  # remove a string in a list 
+print(l1)
