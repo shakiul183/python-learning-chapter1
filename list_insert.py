b081eac34a0e0b77ba078e7ae6c98921550038df
shakiul karim <shakiulkarim8@gmail.com>
2025-10-22 06:29:32 +0600

    how to insert a string in exiting list

---
+++ list_insert.py
@@ -0,0 +1,9 @@
+name = ["shakiul","karim","shohan","10.2365","1245852"] # this is list not string. string can not be change but list can be change
+name.append("daffodil international university")
+print(name)
+
+l1=[1,555,544,54,225,858,45454,2154,]
+l1.sort()
+print(l1) # print sort
+l1.insert(3,3333) # insert such that its index in the list is 3
+print(l1)
