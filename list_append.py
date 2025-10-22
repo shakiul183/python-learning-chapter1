ae47cd4f97c6f5bbf33ec945eeaa0f0a4569a5e1
shakiul karim <shakiulkarim8@gmail.com>
2025-10-22 06:17:13 +0600

    how to append a string

---
+++ list_append.py
@@ -0,0 +1,3 @@
+name = ["shakiul","karim","shohan","10.2365","1245852"] # this is list not string. string can not be change but list can be change
+name.append("daffodil international university")
+print(name)
